19878d2762507134a1cfb79b9c530ea65f7bfe6f
Cynopolis <quinn.henthorne@gmail.com>
2019-12-27 22:34:20 -0600

    Actually Working Code For Xbox Controller
    I got the working code and scaled the xbox input properly

--- Python/Comm.py
+++ Python/Comm.py
@@ -1,4 +1,23 @@
 import serial
+
 class Comm:
-    def __init__(self):
+    def __init__(self, comm_port, baud_rate):
+        try:
+            self.comm = serial.Serial(comm_port, baud_rate, timeout = 1)
+        except:
+            print("That port is not available")
+        return
+
+    def closeSerial(self):
+        self.comm.close()
+        return
+
+    def openSerial(self, path):
+        self.comm.close()
+        self.comm = serial.Serial(path)
+        return
+
+    def writeAnything(self, message):
+        msg = '\r\n' + message
+        self.comm.write(msg.encode())
         return
--- Python/control.py
+++ Python/control.py
@@ -1,14 +1,20 @@
 from inputs import get_gamepad
+from Comm import Comm
 
-def main():
-    while True:
-        try:
-            events = get_gamepad()
-        except:
-            print("No Controller Found!\nExiting...")
-            break
-        for event in events:
-            print(event.ev_type, event.code, event.state)
+x, y = 0, 0
+ser = Comm('COM1', 9600) #the comm port is 100% wrong. Check it and change it.
 
 
-main()
+try:
+    get_gamepad()
+except:
+    print("No Controller Found!\nExiting...")
+    exit()
+while True:
+    events = get_gamepad()
+    for event in events:
+        if event.code == 'ABS_Y':
+            y = round(event.state/32768, 4)
+        elif event.code == 'ABS_X':
+            x = round(event.state/32768, 4)
+    print(x,y)
